b103a886762948453bfe83b6188b353ecd91ba9e
ShorelineCrypto <admin@shorelinecrypto.com>
2021-01-01 17:19:16 -0500

    2020 charts on NENG

--- download_neng_history.sql
+++ download_neng_history.sql
@@ -1,4 +1,4 @@
 .mode csv
 .output NENG_history_trade.csv 
 .open sqlite.db
-select coin, fromSellerToBuyer,time,price from history where coin='NewEnglandcoin' AND coin_pair = 'Dogecoin' ;
+select coin, fromSellerToBuyer,time,price from history where (coin='NewEnglandcoin' OR coin='Nengcoin') AND coin_pair = 'Dogecoin' ;
